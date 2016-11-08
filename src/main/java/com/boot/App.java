2de4565ad5804351aee1eb1f5ec09428252b550e
Madhanraj <phystem2@gmail.com>
2016-11-08 09:50:35 +0530

    Add files via upload

---
+++ src/main/java/com/boot/App.java
@@ -0,0 +1,14 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by upgundecha on 25/03/16.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
