5d7760532abc802f352e1afafaa16de85059793c
Madhanraj <phystem2@gmail.com>
2016-11-24 18:22:47 +0530

    Changed jar to war

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -2,13 +2,21 @@ package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by upgundecha on 25/03/16.
  */
 @SpringBootApplication
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+public class App extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
